ccf7685a4fc5bf1c9dfdb1382b6179e76f3de7c7
Guled Ahmed Guleid <Guled@Guleds-Air.fios-router.home>
2021-05-24 10:28:19 -0400

    refactored code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-/*----- constants -----*/
-/*----- app's state (variables) -----*/
-/*----- cached element references -----*/
-/*----- event listeners -----*/
-/*----- functions -----*/
+const $name = $("#name");
+const $fullName = $("#full-name");
+const $altego = $("#alter-egos");
+const $publisher = $("#publisher");
+const $image = $("#image");
+let characters;
 
-$.ajax("https://www.superheroapi.com/api.php/2813687215612178/search/superman")
-.then(function(data){
-characters = data;
-render();
 
-});
+function handleGetData(event){
+    event.preventDefault();
+    let searchText = $("input#search").val()
+$.ajax({
+    url: `https://www.superheroapi.com/api.php/2813687215612178/search/${searchText}`
+})
+    .then(function (data) {
+        $characters = data;
+        render();
+    },
+    function (error){
+        console.log("bad request: ", error);
+    });
+}
+
+$('form').on('submit', handleGetData);
 
 function render() {
-    const 
+    $name.text($characters.results[0].name);
+    $fullName.text($characters.results[0].biography['full-name']);
+    $altego.text($characters.results[0].biography['alter-egos']);
+    $publisher.text($characters.results[0].biography.publisher);
+    $image.attr("src", $characters.results[0].image.url)
 }
