7d09ca657478bacff1e90e9f87922d53bcc557ab
Guled Ahmed Guleid <Guled@Guleds-Air.fios-router.home>
2021-05-25 10:40:05 -0400

    wip: changed js code to show all values from api

--- js/script.js
+++ js/script.js
@@ -1,32 +1,62 @@
-const $name = $("#name");
-const $fullName = $("#full-name");
-const $altego = $("#alter-egos");
-const $publisher = $("#publisher");
-const $image = $("#image");
+const $container = $('.container')
 let characters;
 
 
-function handleGetData(event){
+function handleGetData(event) {
     event.preventDefault();
     let searchText = $("input#search").val()
-$.ajax({
-    url: `https://www.superheroapi.com/api.php/2813687215612178/search/${searchText}`
-})
-    .then(function (data) {
-        $characters = data;
-        render();
-    },
-    function (error){
-        console.log("bad request: ", error);
-    });
+    $.ajax({
+            url: `https://www.superheroapi.com/api.php/2813687215612178/search/${searchText}`
+        })
+        .then(function (data) {
+                characters = data;
+                render();
+            },
+            function (error) {
+                console.log("bad request: ", error);
+            });
 }
 
 $('form').on('submit', handleGetData);
 
 function render() {
-    $name.text($characters.results[0].name);
-    $fullName.text($characters.results[0].biography['full-name']);
-    $altego.text($characters.results[0].biography['alter-egos']);
-    $publisher.text($characters.results[0].biography.publisher);
-    $image.attr("src", $characters.results[0].image.url)
+    let characterinfo = ''
+    $.each(characters.results, (index, character) => {
+        characterinfo +=
+            `
+        <div class="character-info">
+            <div>
+                <strong>Name:</strong> ${character.name}
+            </div>
+            <div>
+                <br>
+                <strong>Full Name</strong>: ${character.biography['full-name']}
+                </br>
+            </div>
+            <div>
+                <br>
+                <strong>Aliases</strong>: ${character.biography['aliases']}
+                </br>
+            </div>
+            <div>
+            <br>
+                <strong>Alightment</strong>: ${character.biography.alignment}
+            </div>
+
+        </div>
+        <div class="picture">
+            <img id="image" src="${character.image.url}" alt="">
+        </div>
+        `
+    })
+
+    $container.html(characterinfo)
+
 }
+
+
+// $name.html(`<strong>Name</strong>:${character.name}`);
+// $fullName.text(character.biography['full-name']);
+// $altego.text(character.biography['alter-egos']);
+// $publisher.text(character.biography.publisher);
+// $image.attr("src", character.image.url)
